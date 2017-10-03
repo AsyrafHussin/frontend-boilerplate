6f7701bc498aa4cffceb16bb31d55bbbaded95d4
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-10-04 07:48:56 +0800

    Add webpack.mix file

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+ mix.js('src/js/app.js', 'js')    
+     .extract(['jquery', 'bootstrap-sass'])
+     .sass('src/sass/app.scss', 'css')
+     .browserSync('127.0.0.1:8000');
+
